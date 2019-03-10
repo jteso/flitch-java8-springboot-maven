0c8532907cb665d5c0e8b6fe48d72c4667558714
Javier Teso <javier.teso@boral.com.au>
2019-03-10 21:48:53 +1100

    initial commit

---
+++ src/main/java/com/example/hello/HelloFlitch.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFlitch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFlitch.class, args);
+	}
+
+}
